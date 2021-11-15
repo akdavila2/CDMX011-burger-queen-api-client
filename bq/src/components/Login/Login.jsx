98a03ba06f3fb9ac630995e9726a91b02c7c80f4
akdavila2 <anakarina.daviladavila@gmail.com>
2021-11-14 20:51:56 -0600

    validacion de sesion activa metodo onAuthStateChangede

--- bq/src/components/Login/Login.jsx
+++ bq/src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { auth, login } from "../../lib/firebase";
 import logo from "../../assets/banner.png";
 import hamburger from "../../assets/hamburgertwo.png";
@@ -6,10 +6,25 @@ import LoginForm from "./LoginForm";
 import Footer from "../Footer/Footer";
 import { useNavigate } from "react-router-dom";
 import SweetAlert from "sweetalert2";
-
+import { onAuthStateChanged } from "firebase/auth";
 const Login = () => {
   // const [error, setError] = useState("");
   const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/WaiterProfile");
+        const uid = user.uid;
+        console.log("active user", uid);
+      } else {
+        navigate("/");
+        console.log("user not active");
+      }
+    });
+    // eslint-disable-next-line
+  }, []);
+
   const handleSubmit = async (email, password) => {
     const user = auth.currentUser;
     if (user) {
@@ -20,8 +35,7 @@ const Login = () => {
         navigate("/WaiterProfile");
       } catch (error) {
         console.error(error);
-        new SweetAlert('Error', error.message, 'error');
-
+        new SweetAlert("Error", error.message, "error");
         // setError(error.message);
         // setTimeout(() => setError(""), 1500);
       }
