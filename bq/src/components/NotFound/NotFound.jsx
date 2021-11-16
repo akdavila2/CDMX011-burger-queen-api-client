e0c96b7b22e36ce5b098392f426f82ed3ea4c11f
akdavila2 <anakarina.daviladavila@gmail.com>
2021-11-15 20:14:54 -0600

    cambiando imagen de pagina no encontrada

--- bq/src/components/NotFound/NotFound.jsx
+++ bq/src/components/NotFound/NotFound.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import img from "../../assets/error.png"
+import img from "../../assets/notfound.png";
+import Footer from "../Footer/Footer";
 const NotFound = () => {
   return (
-    <div className= "container__notfound">
-      <img src={img} alt="error" />
-    </div>
+    <>
+      <div className="container__notfound">
+        <img src={img} alt="error" />
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 };
 export default NotFound;
