c269e9edb7f8a2d7a2bcbfb5b71548476d4b0901
AngelicaJH <isc.florjardinez@gmail.com>
2021-11-16 23:12:09 -0600

    se termino test de loginForm y se empezo test de login

--- bq/src/__test__/components/LoginForm.test.js
+++ bq/src/__test__/components/LoginForm.test.js
@@ -1,35 +1,36 @@
 import React from "react";
-import { render, screen, fireEvent} from '@testing-library/react';
-
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import LoginForm from "../../components/Login/LoginForm";
 
-beforeEach(()=> render (<LoginForm />))
-test('test to render LoginForm ', () => {
-    const handleLogin = jest.fn();
 
 
-    const contentEmail=screen.getByPlaceholderText('Write your Email')
-    const contentPassword=screen.getByPlaceholderText('Write your Password')
-    const contentSubmit=screen.getByRole('button',{name:/Login/i});
+const mockHandleLogin = jest.fn();
+
+beforeEach(() => render(<LoginForm handleSubmit={mockHandleLogin} />))
+
+
+test('test should render LoginForm ', () => {
+
+    const contentEmail = screen.getByPlaceholderText("Write your Email")
+    const contentPassword = screen.getByPlaceholderText("Write your Password")
+    const contentSubmit = screen.getByRole("button", { name: /Login/i });
 
     expect(contentEmail).toBeInTheDocument()
     expect(contentPassword).toBeInTheDocument()
     expect(contentSubmit).toBeInTheDocument()
 
-    fireEvent.click(contentSubmit)
-     fireEvent.change(contentEmail, {
-        target: {value:'waiterflor@burgerqueen.com'}
+    fireEvent.change(contentEmail, {
+        target: { value: "waiterflor@burgerqueen.com" }
     })
     fireEvent.change(contentPassword, {
-        target: {value:'123456'}
+        target: { value: "123456" }
     })
+    fireEvent.click(contentSubmit)
 
-    
-    // expect(handleSubmit).toHaveBeenCalled()
+    expect(mockHandleLogin).toHaveBeenCalledWith("waiterflor@burgerqueen.com", "123456");
 
-    // fireEvent.submit(contentSubmit)
+});
 
-    // expect(handleSubmit).toHaveBeenCalled()
 
-    expect(handleSubmit).toHaveBeenCalledWith('waiterflor@burgerqueen.com','123456')
-    });
+
+
