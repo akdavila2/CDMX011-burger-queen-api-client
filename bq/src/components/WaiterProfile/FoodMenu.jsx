e644da0bd29955f3b0fea09862894ac1e15e11fe
akdavila2 <anakarina.daviladavila@gmail.com>
2021-11-23 21:24:46 -0600

    se agregan items a order y se pueden eliminar problemas con coffe

--- bq/src/components/WaiterProfile/FoodMenu.jsx
+++ bq/src/components/WaiterProfile/FoodMenu.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
-export const FoodMenu = ({ el }) => {
-  let { name, image } = el;
+export const FoodMenu = ( props ) => {
+  let { product, onAdd } = props;
 
   return (
     <div
       className="content-menu"
       onClick={() => {
-        console.log("estoy clickeando");
+        console.log("clicking");  
+        onAdd(product);
       }}
     >
       <picture className="content__img">
-        <img className="imag-menu" src={image} alt="proof" />
+        <img className="imag-menu" src={product.image} alt={product.image} />
       </picture>
-      <p className="title__food">{name}</p>
+      <p className="title__food">{product.name}</p>
     </div>
   );
 };
--- bq/src/components/WaiterProfile/WaiterProfile.jsx
+++ bq/src/components/WaiterProfile/WaiterProfile.jsx
@@ -4,20 +4,40 @@ import { helpHttp } from "../../helpers/helpHttp.js";
 import DataIteration from "../WaiterProfile/DataIteration";
 import PreLoad from "../PreLoad/PreLoad";
 import NotFound from "../NotFound/NotFound";
-//import CrudApi from "../CrudApi/CrudApi";
 import NavBar from "../NavBar.jsx/NavBar";
 import Footer from "../Footer/Footer";
 import { OrderSummary } from "./OrderSummary";
-// import CrudApiMock from "../ApiMock/CrudApiMock";
-
 
 const WaiterProfile = () => {
   const [db, setDb] = useState(null);
-
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  const [typeFood, setTypeFood] = useState('Breakfast');
+  const [typeFood, setTypeFood] = useState("Breakfast");
+  const [orderItems, setOrderItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = orderItems.find((x) => x.id === product.id);
+    if (exist) {
+      setOrderItems(
+        orderItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setOrderItems([...orderItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = orderItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setOrderItems(orderItems.filter((x) => x.id !== product.id));
+    } else {
+      setOrderItems(
+        orderItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
 
   let url = `http://localhost:5000/product?type=${typeFood}`;
 
@@ -26,7 +46,7 @@ const WaiterProfile = () => {
     helpHttp()
       .get(url)
       .then((res) => {
-        console.log(res);
+        console.log("soy res", res);
         if (!res.err) {
           setDb(res);
           setError(null);
@@ -38,28 +58,41 @@ const WaiterProfile = () => {
       });
   }, [url]);
 
-
   return (
     <div>
       <div className="content__waiter__profile">
         <NavBar />
         <div className="container__menu">
           <section className="column__container">
-            <OrderSummary />
+            <OrderSummary orderItems={orderItems} onRemove={onRemove} />
           </section>
           <section className="column__container">
             {/* <MenuOption /> */}
             <div className="content-menu-option">
               <h3>Menu</h3>
               <section className="section__option">
-                <button className="secondary-button" onClick={()=>{setTypeFood('Breakfast')}}>Breakfast</button>
-                <button className="secondary-button" onClick={()=>{setTypeFood('Lunch')}}>Lunch</button>
+                <button
+                  className="secondary-button"
+                  onClick={() => {
+                    setTypeFood("Breakfast");
+                  }}
+                >
+                  Breakfast
+                </button>
+                <button
+                  className="secondary-button"
+                  onClick={() => {
+                    setTypeFood("Lunch");
+                  }}
+                >
+                  Lunch
+                </button>
               </section>
             </div>
             <div className="container-food">
               {loading && <PreLoad />}
               {error && <NotFound />}
-              {db && <DataIteration data={db} />}
+              {db && <DataIteration products={db} onAdd={onAdd} />}
             </div>
           </section>
         </div>
@@ -68,8 +101,6 @@ const WaiterProfile = () => {
         <Footer />
       </footer>
     </div>
-
-
   );
 };
 export default WaiterProfile;
