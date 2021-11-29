30a2c830b5e845770ca2490a04d264788e1ae18a
akdavila2 <anakarina.daviladavila@gmail.com>
2021-11-29 11:57:39 -0600

    maquetacion HU3

--- bq/src/components/KitchenRoom/FoodOrder.jsx
+++ bq/src/components/KitchenRoom/FoodOrder.jsx
@@ -1,26 +1,60 @@
 import React from "react";
+//import { auth } from "../../lib/firebase";
+import iconUser from "../../assets/user.png";
+import iconClock from "../../assets/clock.png";
 
 export const FoodOrder = (props) => {
   let { order } = props;
+  // const user = auth.currentUser;
 
-  // order.orderItems.map(val=>{
-  //   console.log(val.image)
-  // })
-//https://api-burger-heroku.herokuapp.com/order?status=Pending
+  const date = new Date();
+  console.log("date 1", date);
+  const dateToday = date.toDateString();
+
+  console.log("date", dateToday);
+  console.log(typeof order.id);
+
+  //https://api-burger-heroku.herokuapp.com/order?status=Pending
 
   return (
-    <div>
-      <picture>Foto
-        <img src={order.orderItems.image} alt={order.image} />
-      </picture>
-      <p>ID: {order.id}</p>
-      <p>Customer: {order.userName}</p>
-      <p>Date: {order.dateOrder}</p>
+    <div className="content-order">
+      <header>
+        <div className="content-profile">
+          <img className="icon" src={iconUser} alt="iconUser" />
+          <p>{order.userName}</p>
+        </div>
+        <div className="content-profile">
+          <img className="icon" src={iconClock} alt="iconClock" />
+          <p>10:00</p>
+        </div>
+        <div className="content-profile">
+          <img className="icon" src={iconUser} alt="iconUser" />
+          <p>Flor</p>
+        </div>
+      </header>
+      <section className="content-products">
+        {order.orderItems.map((val) => {
+          return (
+            <section className="content-items">
+              <div className="img-qty">
+                <img className="img-order" src={val.image} alt={val.image} />
+                <p>{val.qty}</p>
+              </div>
+              <div>
+                <p>{val.name}</p>
+              </div>
+            </section>
+          );
+        })}
+      </section>
+
       <button
         onClick={() => {
           console.log("clicking");
         }}
-      >Ready</button>
+      >
+        Ready
+      </button>
     </div>
   );
 };
--- bq/src/components/KitchenRoom/KitchenRoom.jsx
+++ bq/src/components/KitchenRoom/KitchenRoom.jsx
@@ -11,7 +11,7 @@ export const KitchenRoom = () => {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    let url = `https://my-json-server.typicode.com/akdavila2/api_burger/order?status=Pending`;
+    let url = `https://api-burger-heroku.herokuapp.com/order?status=Pending`;
     // let api = helpHttp();
     useEffect(() => {
         setLoading(true);
@@ -34,7 +34,7 @@ export const KitchenRoom = () => {
     <>
       <div className="content-kitchenRoom">
         <NavBar />
-        <h1>Kitchen Room</h1>
+     
         <div className="container-food">
               {loading && <PreLoad />}
               {error && <NotFound />}
