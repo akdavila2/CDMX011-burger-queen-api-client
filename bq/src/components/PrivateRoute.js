3d258a0b12180bfa8baa2888084d5e0eeaf5c4cc
AngelicaJH <isc.florjardinez@gmail.com>
2021-12-08 09:30:32 -0600

    Cambios para login por roles

--- bq/src/components/PrivateRoute.js
+++ bq/src/components/PrivateRoute.js
@@ -1,45 +1,9 @@
-import { useState } from "react";
-import { auth, firestore } from "../lib/firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-//import { Home } from "./Administrator/Home";
-import Login from "./Login/Login";
-//import { UserRegister } from './Administrator/UserRegister';
+import React from "react";
+import {  Navigate } from "react-router-dom";
+import { auth } from "../lib/firebase";
 
-const PrivateRoute = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  const getRol = async (uid) => {
-    const docRef = doc(firestore, `users/${uid}`);
-    const docCipher = await getDoc(docRef);
-    const infoFinish = docCipher.data().rol;
-    return infoFinish;
-  };
-
-  const setUserWithFirebaseAndRol = (userFirebase) => {
-    getRol(userFirebase.uid).then((rol) => {
-      const userData = {
-        uid: userFirebase.uid,
-        email: userFirebase.email,
-        rol: rol,
-      };
-      setUser(userData);
-      console.log("userData finish", userData);
-    });
-  };
-
-  onAuthStateChanged(auth, (userFirebase) => {
-    if (userFirebase) {
-      if (!user) {
-        setUserWithFirebaseAndRol(userFirebase);
-      }
-    } else {
-      setUser(null);
-    }
-  });
-
-  let value = user;
-  console.log("im value", { value });
-  return <>{user ? children : <Login />}</>;
-};
+const PrivateRoute=({ children })=> {
+  const user= auth.currentUser;
+  return user ? children : <Navigate to="/" />;
+}
 export default PrivateRoute;
